995c0455bea690ac1740f985bb6b7e027d3f46b4
Claes-Fredrik Mannby <claes-fredrik.mannby@nuance.com>
2016-01-29 18:13:38 -0800

    Fix non-OS X build

--- sklearn-build-openmp.sh
+++ sklearn-build-openmp.sh
@@ -33,19 +33,10 @@ fi
 case "$OSTYPE" in
   darwin*)
 echo "NOTE: Use e.g. 'brew install openmpi' to install OpenMP support"
-_CXX="gcc-5"
-_CC="gcc-5"
 sed -i -e "s/, '-lrt']/]/g" sklearn-openmp.patch
-;; 
-  *)
-_CXX=$CXX
-_CC=$CC
-;;
-esac
-
 echo "**************************************************************************************************"
 echo "Building scikit-learn with libsvm and liblinear openmp support"
-CXX=$_CXX CC=$_CC CFLAGS="-fopenmp -DCV_OMP=1" CXXFLAGS="-fofopenmp -DCV_OMP=1" LDFLAGS=-lgomp python setup.py build
+CXX="gcc-5" CC="gcc-5" CFLAGS="-fopenmp -DCV_OMP=1" CXXFLAGS="-fofopenmp -DCV_OMP=1" LDFLAGS=-lgomp python setup.py build
 if [ $? -ne 0 ]; then
 	cd ..
     exit 1
@@ -53,7 +44,7 @@ fi
 
 echo "**************************************************************************************************"
 echo "Install scikit-learn"
-CXX=$_CXX CC=$_CC CFLAGS="-fopenmp -DCV_OMP=1" CXXFLAGS="-fofopenmp -DCV_OMP=1" LDFLAGS=-lgomp python setup.py install
+CXX="gcc-5" CC="gcc-5" CFLAGS="-fopenmp -DCV_OMP=1" CXXFLAGS="-fofopenmp -DCV_OMP=1" LDFLAGS=-lgomp python setup.py install
 
 if [ $? -ne 0 ]; then
 	echo "!!!!"
@@ -61,6 +52,27 @@ if [ $? -ne 0 ]; then
 	cd ..
     exit 1
 fi
+;; 
+  *)
+echo "**************************************************************************************************"
+echo "Building scikit-learn with libsvm and liblinear openmp support"
+CFLAGS="-fopenmp -DCV_OMP=1" CXXFLAGS="-fofopenmp -DCV_OMP=1" LDFLAGS=-lgomp python setup.py build
+if [ $? -ne 0 ]; then
+	cd ..
+    exit 1
+fi
 
+echo "**************************************************************************************************"
+echo "Install scikit-learn"
+FLAGS="-fopenmp -DCV_OMP=1" CXXFLAGS="-fofopenmp -DCV_OMP=1" LDFLAGS=-lgomp python setup.py install
+
+if [ $? -ne 0 ]; then
+	echo "!!!!"
+	echo "!!!! Add sudo at beginning of the line below 'Install scikit-learn' in script"
+	cd ..
+    exit 1
+fi
+;;
+esac
 
 cd ..
