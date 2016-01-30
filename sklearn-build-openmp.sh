36dd4e8e85b7dbea19c9361873627a2aae12cd92
Claes-Fredrik Mannby <claes-fredrik.mannby@nuance.com>
2016-01-29 17:29:58 -0800

    Support building on OS X

--- sklearn-build-openmp.sh
+++ sklearn-build-openmp.sh
@@ -30,16 +30,37 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
+case "$OSTYPE" in
+  darwin*)
+echo "NOTE: Use e.g. 'brew install openmpi' to install OpenMP support"
+_CXX="gcc-5"
+_CC="gcc-5"
+sed -i -e "s/, '-lrt']/]/g" sklearn-openmp.patch
+;; 
+  *)
+_CXX=$CXX
+_CC=$CC
+;;
+esac
+
 echo "**************************************************************************************************"
 echo "Building scikit-learn with libsvm and liblinear openmp support"
-CFLAGS="-fopenmp -DCV_OMP=1" CXXFLAGS="-fofopenmp -DCV_OMP=1" LDFLAGS=-lgomp python setup.py build
+CXX=$_CXX CC=$_CC CFLAGS="-fopenmp -DCV_OMP=1" CXXFLAGS="-fofopenmp -DCV_OMP=1" LDFLAGS=-lgomp python setup.py build
 if [ $? -ne 0 ]; then
 	cd ..
     exit 1
 fi
 
 echo "**************************************************************************************************"
-echo "Enter root password for scikit-learn deployment (install script)"
-CFLAGS="-fopenmp -DCV_OMP=1" CXXFLAGS="-fofopenmp -DCV_OMP=1" LDFLAGS=-lgomp python setup.py install
-cd ..
+echo "Install scikit-learn"
+CXX=$_CXX CC=$_CC CFLAGS="-fopenmp -DCV_OMP=1" CXXFLAGS="-fofopenmp -DCV_OMP=1" LDFLAGS=-lgomp python setup.py install
 
+if [ $? -ne 0 ]; then
+	echo "!!!!"
+	echo "!!!! Add sudo at beginning of the line below 'Install scikit-learn' in script"
+	cd ..
+    exit 1
+fi
+
+
+cd ..
